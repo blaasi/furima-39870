866434fb42754b970112f5bd407be88d25058e30
blaasi <katsuyuki2444@gmail.com>
2024-02-15 20:25:52 +0900

    ログイン・ログアウト・新規登録までの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  with_options presence:true,  length: {minimum: 6 }, format: { with: PASSWORD_REGEX, message: 'パスワードには英字と数字の両方を含めて設定してください'} do 
-    validates :encrypted_password 
-  end
-  with_options presence: true, format: { with: /\Aぁ-んァ-ヶ一-龥々ー]+\z/, message:'全角文字を使用してください'} do
+  validates :password, format: { with: PASSWORD_REGEX, message: 'パスワードには英字と数字の両方を含めて設定してください'}
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message:'全角文字を使用してください'} do
     validates :last_name
     validates :first_name
   end
-  with_options presence: true, format: { with: /\Aァ-ヶー]+\z/, message:'全角カタカナ文字を使用してください'} do
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message:'全角カタカナ文字を使用してください'} do
     validates :last_name_kana
     validates :first_name_kana
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_02_14_090049) do
-  create_table "users", charset: "utf8mb4", force: :cascade do |t|
-    t.string "nickname", null: false
+  create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "nickname", null: false
     t.string "last_name", null: false
     t.string "first_name", null: false
     t.string "last_name_kana", null: false
