d1a54a24edc8b73fa3a794a86e9a178e7cab39d8
blaasi <katsuyuki2444@gmail.com>
2024-02-17 11:17:58 +0900

    ActiveHashでcategoryとの連動を作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,14 +3,17 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    redirect_to root_path
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:image)
+    params.require(:item).permit(:image, :category_id)
   end
 end
