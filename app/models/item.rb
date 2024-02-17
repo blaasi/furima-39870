2364702792b7aacf7aa504c10e9a8ad682cd1fef
blaasi <katsuyuki2444@gmail.com>
2024-02-17 14:01:08 +0900

    販売金額の制限の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,7 +13,7 @@ with_options presence: true do
   validates :image
   validates :name
   validates :explanation
-  validates :sale_price, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 9_999_999 },
+  validates :sale_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
   format: { with: /\A[0-9]+\z/ }
 end
 
