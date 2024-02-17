c80f91cb470b27189a44da6e272a503f1d64bfb2
blaasi <katsuyuki2444@gmail.com>
2024-02-17 16:48:36 +0900

    正常系テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    name  {'てすと'}
+    explanation {'てすと'}
+    category_id {'2'}
+    status_id {'2'}
+    shipping_cost_payer_id {'2'}
+    origin_region_id {'2'}
+    days_until_shipping_id {'2'}
+    sale_price {'5000'}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('app/assets/images/star.png'), filename: 'てすと')
+    end
   end
+  
 end
