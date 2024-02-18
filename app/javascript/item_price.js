8196ecd03dbc1d862f89ba4b9dbba3b12548bdcb
blaasi <katsuyuki2444@gmail.com>
2024-02-18 13:25:06 +0900

    販売手数料と販売利益のJSでの表示機能

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,19 @@
 window.addEventListener('turbo:load', () => {
-  console.log("OK");
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const taxRate = 0.10;
+    const taxAmount = inputValue * taxRate;
+    const saleProfit = inputValue - taxAmount;
+
+    console.log(inputValue);
+    console.log(taxAmount);
+    console.log(saleProfit);
+
+  const addTaxDom = document.getElementById("add-tax-price");
+  const saleProfitDom = document.getElementById("profit");
+
+  addTaxDom.innerHTML = Math.floor(taxAmount)
+  saleProfitDom.innerHTML = Math.floor(saleProfit)
+  });
 });
