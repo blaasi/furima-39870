06482d757a00749c7561f10d50a94ee919365aec
blaasi <katsuyuki2444@gmail.com>
2024-02-18 14:18:11 +0900

    金額のバリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,8 +13,7 @@ with_options presence: true do
   validates :image
   validates :name
   validates :explanation
-  validates :sale_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-  format: { with: /\A[0-9]+\z/ }
+  validates :sale_price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
 
   with_options presence: true, numericality: { other_than: 1 , message: "can't be blank"} do
