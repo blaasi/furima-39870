819b8ebf8451a9623d659fe166092e81041aed3f
blaasi <katsuyuki2444@gmail.com>
2024-02-21 00:59:02 +0900

    ログイン状態での売却済み商品と自分で出品した商品の購入画面への遷移のバリデーション設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,13 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
   before_action :move_to_new_user_session_path, only: [:index]
+  before_action :move_to_root_path, only: [:index]
 
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       @order_address.save
@@ -23,10 +23,19 @@ class OrdersController < ApplicationController
     params.require(:order_address).permit(:postcode, :origin_region_id, :city, :street_address, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
   def move_to_new_user_session_path
     return if user_signed_in?
 
     redirect_to new_user_session_path
   end
 
+  def move_to_root_path
+    if current_user == @item.user || @item.sold_out?
+      redirect_to root_path
+    end
+  end
 end
