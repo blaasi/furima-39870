8a25d581ac2cbafc8f747157bf288ed783d5194f
blaasi <katsuyuki2444@gmail.com>
2024-02-20 17:07:21 +0900

    バリデーションの修正とsaveの設定

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,18 +2,18 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postcode, :origin_region_id, :city, :street_address, :building_name, :telephone_number
 
-  # ここにバリデーションの処理を書く
   with_options presence: true do
     validates :user_id
     validates :item_id
     validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :origin_region_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :city
-    validates :street_address
     validates :telephone_number, format: {with: /\A[0-9]{10,11}\z/, message: "Only half-width numeric values with 10 to 11 digits are allowed."}
   end
 
   def save
-    # 各テーブルにデータを保存する処理を書く
+    order = Order.create(user_id: user_id, item_id: item_id)
+
+    Addres.create(postcode: postcode, origin_region_id: origin_region_id, city: city, street_address: street_address, telephone_number: telephone_number, order_id: order.id)
   end
 end
