d36ca0e3f91f9b5afb174323d605c9e79f78c32c
blaasi <katsuyuki2444@gmail.com>
2024-02-21 16:20:54 +0900

    販売手数料等の表示のrenderの際の挙動修正と端数処理の修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,15 +1,19 @@
-window.addEventListener('turbo:load', () => {
+const price = () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
     const taxRate = 0.10;
     const taxAmount = inputValue * taxRate;
-    const saleProfit = inputValue - taxAmount;
+    const taxAmount_integer = Math.floor(taxAmount);
+    const saleProfit = inputValue - taxAmount_integer;
 
   const addTaxDom = document.getElementById("add-tax-price");
   const saleProfitDom = document.getElementById("profit");
 
-  addTaxDom.innerHTML = Math.floor(taxAmount)
-  saleProfitDom.innerHTML = Math.floor(saleProfit)
+  addTaxDom.innerHTML = taxAmount_integer;
+  saleProfitDom.innerHTML = saleProfit;
   });
-});
+};
+
+window.addEventListener('turbo:load',price);
+window.addEventListener('turbo:render',price);
