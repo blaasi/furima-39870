65c58181aa981243f3afc2e9c40d59906baf90ff
blaasi <katsuyuki2444@gmail.com>
2024-02-21 10:40:54 +0900

    カード情報のトークン化

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,25 @@
 const pay = () => {
-  console.log("カード情報トークン化のためのJavaScript");
+  const payjp = Payjp('pk_test_f75c6ce5aef83c79528194f5')// PAY.JPテスト公開鍵
+  const elements = payjp.elements();
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc');
+
+  numberElement.mount('#number-form');
+  expiryElement.mount('#expiry-form');
+  cvcElement.mount('#cvc-form');
+
+  const form = document.getElementById('charge-form')
+  form.addEventListener("submit", (e) => {
+    payjp.createToken(numberElement).then(function (response) {
+      if (response.error) {
+      } else {
+        const token = response.id;
+        console.log(token)
+      }
+    });
+    e.preventDefault();
+  });
 };
 
 window.addEventListener("turbo:load", pay);
